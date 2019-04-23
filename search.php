85f40be780270a4997eaa6d49cd8ee6e4cf09a47
averagebear <markjriley1899@gmail.com>
2019-04-23 11:01:22 +0100

    Made changes to CSS

--- search.php
+++ search.php
@@ -8,7 +8,7 @@ $search=$_POST['search'];
 <head>
 <title>Universities</title>
 <script src="jquery-3.3.1.min.js"></script>
-
+<link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
 <link href="styleSheet.css" rel="stylesheet" type="text/css">
 </head>
 <div class="topnav" id="myTopnav">
@@ -20,26 +20,24 @@ $search=$_POST['search'];
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="overallStats">Overall</a>
-      <a href="agricultureStats">Agriculture</a>
-	  <a href="educationStats">Education</a>
-	  <a href="engineeringStats">Engineering, Manufacturing & Construction</a>
-	  <a href="healthStats">Health & Welfare</a>
-	  <a href="humanitiesStats">Humanities & Arts</a>
-	  <a href="scienceStats">Science</a>
-	  <a href="servicesStats">Services</a>
-	  <a href="socialStats">Social Sciences, Business & Law</a>
+      <a href="overallStats.html">Overall</a>
+      <a href="agricultureStats.html">Agriculture</a>
+	  <a href="educationStats.html">Education</a>
+	  <a href="engineeringStats.html">Engineering, Manufacturing & Construction</a>
+	  <a href="healthStats.html">Health & Welfare</a>
+	  <a href="humanitiesStats.html">Humanities & Arts</a>
+	  <a href="scienceStats.html">Science</a>
+	  <a href="servicesStats.html">Services</a>
+	  <a href="socialStats.html">Social Sciences, Business & Law</a>
     </div>
   </div> 
   <a href="opportunities.html">Opportunities</a>
   <a href="javascript:void(0);" class="icon" onclick="navbar()">&#9776;</a>
 </div>
+
 <body>
-
-
 <div class = "row" id = "content" style="padding: 10px;">
 </div>
-
 </body>
 
 </html>
@@ -56,17 +54,14 @@ $.ajax({
 			var job = document.createElement("div");
 			job.className = "jobDiv col-4";
 			contentDiv.appendChild(job);
-			
 			var header = document.createElement("p");
 			header.innerHTML = result.title;
 			header.className = "jobHeader";
 			job.appendChild(header);
-			
 			var body = document.createElement("p");
 			body.innerHTML = result.summary;
 			body.className = "jobBody";
 			job.appendChild(body);
-			
 			var link = document.createElement("a");
 			link.innerHTML = "Link to Site";
 			link.target = "_blank";
@@ -75,10 +70,7 @@ $.ajax({
 			job.appendChild(link);
 			
 		});
-	
-	
 	}
-	
 });
 
 /* NAVBAR */
